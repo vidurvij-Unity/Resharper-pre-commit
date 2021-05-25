1fdad72831dde590be249e313b147779c8918531
vidurvij-Unity <60901103+vidurvij-Unity@users.noreply.github.com>
2021-05-25 08:43:31 -0700

    Update ResharperPreCommit.sh

--- ResharperPreCommit.sh
+++ ResharperPreCommit.sh
@@ -2,8 +2,16 @@
 
 set -x
 echo "Formatting files..."
+
+
+STAGED_FILES=`git diff --name-only --cached --diff-filter=d`
+echo "Staged files ${STAGED_FILES}"
+
+
 export PATH="$PATH:/Users/$USER/.dotnet/tools"
 SOLUTION_FILE=$(find . -type f -name "*.sln")
 git clone https://github.com/vidurvij-Unity/Resharper-pre-commit.git /Users/$USER/Desktop 
 jb cleanupcode $SOLUTION_FILE --settings="/Users/$USER/Desktop/Resharper-pre-commit/Unity.Global.DotSettings"
-git add -u
+
+echo "Restaging files: $STAGED_FILES"
+echo ${STAGED_FILES} | xargs -t -l git add
