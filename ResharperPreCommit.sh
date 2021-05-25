ebc2ff3be6b1c331d94c2fda20995ebf16d8d6d2
vidurvij-Unity <60901103+vidurvij-Unity@users.noreply.github.com>
2021-05-25 08:04:52 -0700

    Update ResharperPreCommit.sh

--- ResharperPreCommit.sh
+++ ResharperPreCommit.sh
@@ -6,4 +6,4 @@ export PATH="$PATH:/Users/$USER/.dotnet/tools"
 SOLUTION_FILE=$(find . -type f -name "*.sln")
 git clone https://github.com/vidurvij-Unity/Resharper-pre-commit.git /Users/$USER/Desktop 
 jb cleanupcode $SOLUTION_FILE --settings="/Users/$USER/Desktop/Resharper-pre-commit/Unity.Global.DotSettings"
-git add -u && !!  
+git add -u
