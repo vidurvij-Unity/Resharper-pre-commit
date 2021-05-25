c995a6f4138b5e27d5099019a25183d4346f7fe0
vidurvij-Unity <60901103+vidurvij-Unity@users.noreply.github.com>
2021-05-25 07:09:30 -0700

    Adding DotSettingsFile

--- ResharperPreCommit.sh
+++ ResharperPreCommit.sh
@@ -4,4 +4,4 @@ set -x
 echo "Formatting files..."
 export PATH="$PATH:/Users/$USER/.dotnet/tools"
 SOLUTION_FILE=$(find . -type f -name "*.sln")
-jb cleanupcode $SOLUTION_FILE -dsl="SolutionShared"
+jb cleanupcode $SOLUTION_FILE --settings="Unity.Global.DotSettings"
