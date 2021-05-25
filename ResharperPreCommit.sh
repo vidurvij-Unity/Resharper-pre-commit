476fa2132435b65174d46c2e5ebfec880b705c62
vidurvij-Unity <60901103+vidurvij-Unity@users.noreply.github.com>
2021-05-25 07:30:22 -0700

    Update ResharperPreCommit.sh

--- ResharperPreCommit.sh
+++ ResharperPreCommit.sh
@@ -4,4 +4,5 @@ set -x
 echo "Formatting files..."
 export PATH="$PATH:/Users/$USER/.dotnet/tools"
 SOLUTION_FILE=$(find . -type f -name "*.sln")
-jb cleanupcode $SOLUTION_FILE --settings="Unity.Global.DotSettings"
+git clone https://github.com/vidurvij-Unity/Resharper-pre-commit.git /Users/$USER/Desktop 
+jb cleanupcode $SOLUTION_FILE --settings="/Users/$USER/Desktop/Resharper-pre-commit/Unity.Global.DotSettings"  
